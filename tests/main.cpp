407d5c53ee07da3110e16e53e192e9860e2f1db7
timlathy <tim.labushev@gmail.com>
2019-12-19 15:47:49 +0300

    add test config

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,3 +1,16 @@
 #define CATCH_CONFIG_MAIN
 
 #include <catch2/catch.hpp>
+
+struct TestSetupListener : Catch::TestEventListenerBase
+{
+    using TestEventListenerBase::TestEventListenerBase;
+
+    void testRunStarting(Catch::TestRunInfo const& testRunInfo) override
+    {
+        setenv("ASM_DBG_CONFIG", "tests/fixtures/config.toml", 1);
+        setenv("HSA_TOOLS_LIB", "build/src/libplugintercept.so", 1);
+    }
+};
+
+CATCH_REGISTER_LISTENER(TestSetupListener)
